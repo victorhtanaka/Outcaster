b3a00d359122fdd8bf48f90ca041916ab4e77946
victorhtanaka <victorhtanaka555@gmail.com>
2023-05-19 15:33:15 -0300

    mudei uns bagulho

--- ui.py
+++ ui.py
@@ -9,8 +9,22 @@ class UI:
         self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
 
         #SETUP DA BARRA DE COISAS
-        self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
+        self.health_bar_rect = pygame.Rect(10,100,HEALTH_BAR_WIDTH,BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+        
+    def show_bar(self,current,max_amount,bg_rect,color):
+        #draw background
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
+
+        #converter statistica para pixel
+        ratio = current / max_amount
+        current_width = bg_rect.width * ratio
+        current_rect = bg_rect.copy()
+        current_rect.width = current_width
+
+        #draw the bar
+        pygame.draw.rect(self.display_surface,color,current_rect)
 
     def display(self,player):
-        
+        self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
+        self.show_bar(player.energy,player.stats['energy'],self.health_bar_rect,ENERGY_COLOR)
