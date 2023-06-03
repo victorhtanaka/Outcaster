45c3b1baba271d5d793f094610247457c8bbaa93
victorhtanaka <victorhtanaka555@gmail.com>
2023-06-03 09:35:01 -0300

    ut

--- code/player.py
+++ code/player.py
@@ -6,7 +6,7 @@ from entity import Entity
 class Player(Entity):
     def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack, create_magic):
         super().__init__(groups)
-        self.image = pygame.image.load('gameinfo/graphics/player/down/down_0.png').convert_alpha()
+        self.image = pygame.image.load('gameinfo/graphics/player/down/down (1).png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(-10, HITBOX_OFFSET['player'])
 
